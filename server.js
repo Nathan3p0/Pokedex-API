a88dd422f2e0aa0e498d5663cc316d6e57faeccd
Nathan Szelag <nszelag@live.com>
2019-08-01 10:59:00 -0400

    Created initial server script

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use((req, res) => {
+    res.send('Hello, World!!');
+});
+
+const port = 8000;
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`)
+});
