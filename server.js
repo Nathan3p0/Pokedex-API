87924e8434a7d019a03db25afe215be0118ee8c0
Nathan Szelag <nszelag@live.com>
2019-08-01 12:42:06 -0400

    added authentication and routes

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 
+console.log(process.env.API_TOKEN);
+
 const app = express();
+
+app.use(function validateBearerToken(req, res, next) {
+    console.log('validate bearer token middleware')
+    const authToken = req.get('Authorization');
+    const apiToken = process.env.API_TOKEN;
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+
+    next()
+})
 
 app.use(morgan('dev'));
 
-app.use((req, res) => {
-    res.send('Hello, World!!');
-});
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
+
+const handleGetTypes = (req, res) => {
+    res.json(validTypes);
+}
+
+app.get('/types', handleGetTypes);
+
+const handleGetPokemon = (req, res) => {
+    res.send('Hello Pokemon!');
+}
+
+app.get('/pokemon', handleGetPokemon);
 
 const port = 8000;
 
