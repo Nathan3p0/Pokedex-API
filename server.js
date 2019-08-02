50d34a6e7686f75d2f0283960df154fa56a59f46
Nathan Szelag <nszelag@live.com>
2019-08-01 22:37:53 -0400

    added authorization

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 const POKEDEX = require('./pokedex.json');
 
 console.log(process.env.API_TOKEN);
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
 app.use(function validateBearerToken(req, res, next) {
     console.log('validate bearer token middleware')
     const authToken = req.get('Authorization');
